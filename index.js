15d1c3c899a187c47b4926827d5f01fccdb5124f
tfkhdyt <ta142fa@gmail.com>
2021-12-22 10:07:51 +0700

    nambah delete

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 const { ApolloServer, gql } = require("apollo-server");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const uri = 'mongodb+srv://tfkhdyt:oxsNzDKhjNPDQ@cluster0.pbe5r.mongodb.net/belajar-graphql?retryWrites=true&w=majority'
+const uri =
+  "mongodb+srv://tfkhdyt:oxsNzDKhjNPDQ@cluster0.pbe5r.mongodb.net/belajar-graphql?retryWrites=true&w=majority";
 
 mongoose.connect(uri, (err) => {
-  if (err) return err
-  console.log('MongoDB is connected...')
-})
+  if (err) return err;
+  console.log("MongoDB is connected...");
+});
 
-const Mahasiswa = mongoose.model('Mahasiswa', {
-  nim: {
-    type: String,
-    required: true
+const Mahasiswa = mongoose.model(
+  "Mahasiswa",
+  {
+    nim: {
+      type: String,
+      required: true,
+    },
+    nama: {
+      type: String,
+      required: true,
+    },
+    prodi: {
+      type: String,
+      required: true,
+    },
   },
-  nama: {
-    type: String,
-    required: true
-  },
-  prodi: {
-    type: String,
-    required: true
-  },
-}, 'mahasiswa')
+  "mahasiswa"
+);
 
 const typeDefs = gql`
   type Mahasiswa {
     nim: ID!
-    nama: String!
-    prodi: String!
-  }
-
-  type UpdateMhs {
-    nim: ID!
     nama: String
     prodi: String
-  }
-
-  type DeleteMhs {
-    msg: String!
-    nim: ID!
   }
 
   type Query {
@@ -47,30 +41,35 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addMhs(nama: String!, nim: ID!, prodi: String!): Mahasiswa!
-    updateMhs(nim: ID!, nama: String, prodi: String): String
-    deleteMhs(nim: ID): DeleteMhs
+    addMhs(nama: String!, nim: ID!, prodi: String!): Mahasiswa
+    updateMhs(nim: ID!, nama: String, prodi: String): Mahasiswa
+    deleteMhs(nim: ID): Mahasiswa
   }
 `;
 
 const resolvers = {
   Query: {
-    mahasiswa: async () => await Mahasiswa.find(),
-    mhs: async (_, { nim }) =>
-      await Mahasiswa.findOne({ nim })
+    mahasiswa: async () => await Mahasiswa.find().sort('nim'),
+    mhs: async (_, { nim }) => await Mahasiswa.findOne({ nim }),
   },
+
   Mutation: {
     addMhs: async (_, args) => {
-      const newMhs = args
       await Mahasiswa.insertMany(args)
-      return newMhs
+      return args
     },
+
     updateMhs: async (_, args) => {
-      const Mhs = await Mahasiswa.findOne({ nim: args.nim })
+      const Mhs = await Mahasiswa.findOne({ nim: args.nim });
       await Mahasiswa.findByIdAndUpdate(Mhs._id, args)
-      return `[${Mhs.nim}] ${Mhs.nama} berhasil diupdate!`
+      return args
+    },
+    deleteMhs: async (_, args) => {
+      const Mhs = await Mahasiswa.findOne({ nim: args.nim });
+      await Mahasiswa.findByIdAndDelete(Mhs._id)
+      return Mhs
     }
-  }
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
